364c7d05d2fab268650695c55a9de5a9fcf79968
Vishwas Paikra <vishwaspaikra007@gmail.com>
2022-06-12 22:16:41 +0530

    cors firebase url

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ const passport = require('./routes/passport')
 require('./routes/customPassportAuthenticate')(passport)
 const cors = require('cors')
 app.use(cors(
-    {origin: ['http://127.0.0.1:3000', 'http://localhost:3000', 'https://vishwaspaikra007.github.io'],
+    {origin: ['http://127.0.0.1:3000', 
+    'http://localhost:3000', 
+    'https://vishwaspaikra007.github.io', 
+    'https://adrixus-fronend.web.app/',
+    'https://adrixus-fronend.firebaseapp.com/'],
      credentials: true}
      ))
 app.options('*', cors())  // enable pre-flight request for complex cors request for every route
