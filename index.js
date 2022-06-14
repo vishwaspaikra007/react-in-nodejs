9689514659af5eb3832468d4c516e32b1784882c
Vishwas Paikra <vishwaspaikra007@gmail.com>
2022-06-14 10:33:04 +0530

    react in nodejs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 
-// const path = require('path')
+const path = require('path')
 // const cookieParser = require('cookie-parser')
 const http = require('http')
 const server = http.createServer(app)
@@ -13,9 +13,13 @@ const getUsers = require('./routes/getUsers')
 const passport = require('./routes/passport')
 require('./routes/customPassportAuthenticate')(passport)
 const cors = require('cors')
+// app.use(cors())
 app.use(cors(
-    {origin: ['http://127.0.0.1:3000', 
+    {origin: [
+    'http://127.0.0.1:3000', 
+    'http://127.0.0.1:5000', 
     'http://localhost:3000', 
+    'http://localhost:5000', 
     'https://vishwaspaikra007.github.io', 
     'https://adrixus-fronend.web.app',
     'https://adrixus-fronend.firebaseapp.com'],
@@ -30,6 +34,18 @@ app.use(passport.initialize())
 app.use(signup)
 app.use(login)
 app.use(getUsers)
+
+app.use(express.static(path.join(__dirname, "./client/build")))
+app.get("/", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "./client/build/index.html")
+    );
+});
+app.get("*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "./client/build/index.html")
+    );
+});
 
 const PORT = process.env.PORT || 5000
 server.listen(PORT, () => {
